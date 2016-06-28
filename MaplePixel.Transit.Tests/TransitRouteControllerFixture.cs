aaf13510d269b8c52d8d12df9e806e127701f5e3
Rob Pearson <robpearson@fastmail.net>
2016-06-28 22:03:04 +1000

    Updated controller w/ test data and added inital tests.

--- MaplePixel.Transit.Tests/TransitRouteControllerFixture.cs
+++ MaplePixel.Transit.Tests/TransitRouteControllerFixture.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using MaplePixel.Transit.Controllers;
+using NUnit.Framework;
 
 namespace MaplePixel.Transit.Tests
 {
@@ -6,10 +8,28 @@ namespace MaplePixel.Transit.Tests
     public class TransitRouteControllerFixture
     {
         [Test]
-        public void DoesNothing()
+        public void Can_Get_All_Transit_Routes()
         {
-            // Given / When / Then
-            Assert.That(true, Is.True);
+            // Given / When
+            var controller = new TransitRouteController();
+            var allRoutes = controller.Get().ToList();
+
+            Assert.That(allRoutes, Has.Count.GreaterThan(0));
+        }
+
+        [Test]
+        public void Can_Get_Transit_Route_By_Id()
+        {
+            // Given 
+            const int id = 1;
+
+            // When
+            var controller = new TransitRouteController();
+            var route = controller.Get(id);
+
+            Assert.That(route.Id, Is.EqualTo(1));
+            Assert.That(route.LongName, Is.EqualTo("Caboolture Line"));
+            Assert.That(route.ShortName, Is.EqualTo("CAB"));
         }
     }
 }
--- MaplePixel.Transit/Controllers/TransitRouteController.cs
+++ MaplePixel.Transit/Controllers/TransitRouteController.cs
@@ -1,35 +1,53 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http;
+using MaplePixel.Transit.Models;
 
 namespace MaplePixel.Transit.Controllers
 {
     public class TransitRouteController : ApiController
     {
-        // GET: api/TransitRoute
-        public IEnumerable<string> Get()
+        private readonly List<TransitRoute> transitRoutes = new List<TransitRoute>();
+
+        public TransitRouteController()
         {
-            return new string[] { "value1", "value2" };
+            // Test Data
+            var route1 = new TransitRoute {Id = 1, LongName = "Caboolture Line", ShortName = "CAB"};
+            var route2 = new TransitRoute {Id = 2, LongName = "Ipswitch Line", ShortName = "IPS"};
+            var route3 = new TransitRoute {Id = 3, LongName = "Ferny Grove Line", ShortName = "FER"};
+            transitRoutes.Add(route1);
+            transitRoutes.Add(route2);
+            transitRoutes.Add(route3);
+        }
+
+        // GET: api/TransitRoute
+        public IEnumerable<TransitRoute> Get()
+        {
+            return transitRoutes;
         }
 
         // GET: api/TransitRoute/5
-        public string Get(int id)
+        public TransitRoute Get(int id)
         {
-            return "value";
+            return transitRoutes.FirstOrDefault(r => r.Id == id);
         }
 
         // POST: api/TransitRoute
         public void Post([FromBody]string value)
         {
+            // TODO: Add support to create a new transit route
         }
 
         // PUT: api/TransitRoute/5
         public void Put(int id, [FromBody]string value)
         {
+            // TODO: Add support to update a new transit route
         }
 
         // DELETE: api/TransitRoute/5
         public void Delete(int id)
         {
+            // TODO: Add support to delete a new transit route
         }
     }
 }
